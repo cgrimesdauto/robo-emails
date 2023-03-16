01cc5f40e3b245ec321714b92b445b0aca5d7e5a
cgrimesdauto <127969427+cgrimesdauto@users.noreply.github.com>
2023-03-16 09:13:56 -0400

    format signature

--- index.js
+++ index.js
@@ -46,7 +46,6 @@ function test(){
   //generate the html
   var outputHTML = `<table style="background-color: #003c5e; width: 600px; border: 0; border-collapse: collapse; font-family: Tahoma, Arial, sans-serif;">
   <tbody>
-
     <tr>
       <td style="background-color: #00263c; padding: 20px; text-align: center; border: 1px solid #003c5e; width: 799.231px;">
         <img src="https://robolive.info/wp-content/uploads/2023/03/Robolive-Logo_White_WithoutProtectionZone-1.png" style="width:75%; padding:20px;">
@@ -55,48 +54,42 @@ function test(){
     </tr>
     <tr>
       <td style="background-color: #ffffff; padding: 30px 20px 20px 20px; border: 1px solid #003c5e; width: 100%;">
-
         <p style="font-family: Tahoma, Arial, sans-serif; text-align:center;">${opening}</p>
         <table style="width: 100%; border: 0; border-collapse: collapse;">
           <tbody>
-
             <tr valign="top" style="padding: 20px;">
-
               <td style="width: calc(100% /3); vertical-align: top; padding: 30px 10px 30px 10px;text-align:center;"><a href="${news1Link}"><img src="${news1SRC}" alt="News Image" style="width: 150px; height: auto; margin-bottom: 10px;" /></a>
                 <h2 style="margin-bottom: 10px; font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:18px;">${news1Title}</h2>
                 <p style="font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:14px;">${news1Body}</p>
                 <center> <a href="${news1Link}" style="font-family: Tahoma, Arial, sans-serif; color: #65a4d4; margin-top: 10px;">Read more</a></center>
               </td>
-
               <td style="width: calc(100% /3); vertical-align: top; padding: 30px 10px 30px 10px;text-align:center;"><a href="${news2Link}"><img src="${news2SRC}" alt="News Image" style="width: 150px; height: auto; margin-bottom: 10px;" /></a>
                 <h2 style="margin-bottom: 10px; font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:18px;">${news2Title}</h2>
                 <p style="font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:14px;">${news2Body}</p>
                 <center> <a href="${news2Link}" style="font-family: Tahoma, Arial, sans-serif; color: #65a4d4; margin-top: 10px;">Read more</a></center>
               </td>
-
               <td style="width: calc(100% /3); vertical-align: top; padding: 30px 10px 30px 10px;text-align:center;"><a href="${news3Link}"><img src="${news3SRC}" alt="News Image" style="width: 150px; height: auto; margin-bottom: 10px;" /></a>
                 <h2 style="margin-bottom: 10px; font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:18px;">${news3Title}</h2>
                 <p style="font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:14px;">${news3Body}</p>
                 <center> <a href="${news3Link}" style="font-family: Tahoma, Arial, sans-serif; color: #65a4d4; margin-top: 10px;">Read more</a></center>
               </td>
-
             </tr>
           </tbody>
         </table>
         <p style="font-family: Tahoma, Arial, sans-serif; margin-top: 20px; margin-bottom:50px; text-align:center;">${closing}</p>
-
       </td>
     </tr>
     <tr>
-      <td style="background-color: #00263c; padding: 30px 30px; text-align: center; border: 1px solid #003c5e; width: 100%;">
-        <p style="font-size: 13px; color: #ffffff;"><span style="font-size: 10pt; font-family: tahoma ,arial ,helvetica ,sans-serif;"><strong>Dressler Automation | We Set the Standards<br />RoboLive<sup>&reg;</sup>&nbsp;|&nbsp;Transparency for Production Robots</strong><br /><br />Dressler Automation Corp<br />102 Jordan Drive, Chattanooga TN 37421<br>Tel.: (423) 661-2088<br />E-Mail:&nbsp;<a href="mailto:deu@dautomation.com" style="color: #65a4d4">usa@dautomation.com</a><br /><br /><a href="https://dautomation.com/" style="color: #65a4d4">dautomation.com</a>&nbsp;|&nbsp;<a href="https://www.linkedin.com/company/42300673/" style="color: #65a4d4">LinkedIn</a><br /><a href="https://robolive.info/" style="color: #65a4d4">robolive.info</a>&nbsp;|&nbsp;<a href="https://www.linkedin.com/showcase/77684161/" style="color: #65a4d4">LinkedIn</a><br /><br /></span></p>
+            <td style="background-color: #00263c; padding: 30px 30px; text-align: center; border: 1px solid #003c5e; width: 100%;">
+        <p style="font-size: 13px; color: #ffffff;"><span style="font-size: 10pt; font-family: tahoma ,arial ,helvetica ,sans-serif;"><strong>RoboLive<sup>&reg;</sup>&nbsp;|&nbsp;Transparency for Production Robots</strong><br /><br />Dressler Automation Corp<br />102 Jordan Drive, Chattanooga TN 37421<br>Tel.: (423) 661-2088<br />E-Mail:&nbsp;<a href="mailto:deu@dautomation.com" style="color: #65a4d4">usa@dautomation.com</a><br /><br />
+
+          
+          <a href="https://robolive.info/" style="color: #65a4d4">robolive.info</a>&nbsp;|&nbsp;<a href="https://www.linkedin.com/showcase/77684161/" style="color: #65a4d4">LinkedIn</a><br /><br /></span></p>
         <p style="font-size: 13px; color: #ffffff;"><a href="https://calendly.com/robolivedeu" style="background-color: #ffffff; color: #003c5e; padding: 10px 20px; text-decoration: none; border-radius: 30px; font-size: 14px; font-weight: bold;">Request a demo</a></p>
-        
-        
+
         <!-- Ende der Signatur -->
-        
-        
-        <p style="font-size: 13px; color: #ffffff;"><span style="font-size: 10pt; font-family: tahoma ,arial ,helvetica ,sans-serif;"><br />&copy; Robolive 2023 - All rights reserved.<br />Don't want to recieve further communication? <a href="#" style="color:#fff;">Click here to unsubscribe</a>.</span></p>
+
+        <p style="font-size: 13px; color: #ffffff;"><span style="font-size: 10pt; font-family: tahoma ,arial ,helvetica ,sans-serif;"><br />&copy; Robolive 2023 - All rights reserved.<br />Don't want to receive further communication? <a href="#" style="color:#fff;">Click here to unsubscribe</a>.</span></p>
       </td>
     </tr>
   </tbody>
@@ -104,7 +97,6 @@ function test(){
   
   var outputHTMLDE = `<table style="background-color: #003c5e; width: 600px; border: 0; border-collapse: collapse; font-family: Tahoma, Arial, sans-serif;">
   <tbody>
-
     <tr>
       <td style="background-color: #00263c; padding: 20px; text-align: center; border: 1px solid #003c5e; width: 799.231px;">
         <img src="https://robolive.info/wp-content/uploads/2023/03/Robolive-Logo_White_WithoutProtectionZone-1.png" style="width:75%; padding:20px;">
@@ -113,36 +105,29 @@ function test(){
     </tr>
     <tr>
       <td style="background-color: #ffffff; padding: 30px 20px 20px 20px; border: 1px solid #003c5e; width: 100%;">
-
         <p style="font-family: Tahoma, Arial, sans-serif; text-align:center;">${opening}</p>
         <table style="width: 100%; border: 0; border-collapse: collapse;">
           <tbody>
-
             <tr valign="top" style="padding: 20px;">
-
               <td style="width: calc(100% /3); vertical-align: top; padding: 30px 10px 30px 10px;text-align:center;"><a href="${news1Link}"><img src="${news1SRC}" alt="News Image" style="width: 150px; height: auto; margin-bottom: 10px;" /></a>
                 <h2 style="margin-bottom: 10px; font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:18px;">${news1Title}</h2>
                 <p style="font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:14px;">${news1Body}</p>
                 <center> <a href="${news1Link}" style="font-family: Tahoma, Arial, sans-serif; color: #65a4d4; margin-top: 10px;">Mehr lesen</a></center>
               </td>
-
               <td style="width: calc(100% /3); vertical-align: top; padding: 30px 10px 30px 10px;text-align:center;"><a href="${news2Link}"><img src="${news2SRC}" alt="News Image" style="width: 150px; height: auto; margin-bottom: 10px;" /></a>
                 <h2 style="margin-bottom: 10px; font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:18px;">${news2Title}</h2>
                 <p style="font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:184x;">${news2Body}</p>
                 <center> <a href="${news2Link}" style="font-family: Tahoma, Arial, sans-serif; color: #65a4d4; margin-top: 10px;">Mehr lesen</a></center>
               </td>
-
               <td style="width: calc(100% /3); vertical-align: top; padding: 30px 10px 30px 10px;text-align:center;"><a href="${news3Link}"><img src="${news3SRC}" alt="News Image" style="width: 150px; height: auto; margin-bottom: 10px;" /></a>
                 <h2 style="margin-bottom: 10px; font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:18px;">${news3Title}</h2>
                 <p style="font-family: Tahoma, Arial, sans-serif;text-align:center;font-size:14px;">${news3Body}</p>
                 <center> <a href="${news3Link}" style="font-family: Tahoma, Arial, sans-serif; color: #65a4d4; margin-top: 10px;">Mehr lesen</a></center>
               </td>
-
             </tr>
           </tbody>
         </table>
         <p style="font-family: Tahoma, Arial, sans-serif; margin-top: 20px; margin-bottom:50px; text-align:center;">${closing}</p>
-
       </td>
     </tr>
     <tr>
